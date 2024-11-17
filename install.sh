a08df93d1bf7b6da334345021f41c491904f16ad
dongqianggo <1012082932@qq.com>
2024-11-17 09:29:39 +0800

    update

--- install.sh
+++ install.sh
@@ -40,8 +40,6 @@ echo " installed coin-prediction-reputer end!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo " installed basic-coin-prediction-node begin!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 git clone https://github.com/dongqianggo/basic-coin-prediction-node.git
 cd basic-coin-prediction-node
-cp .env.example .env
-cp config.example.json config.json
 chmod +x init.config
 ./init.config
 docker compose up -d
