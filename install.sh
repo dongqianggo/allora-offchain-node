848e6f471d8147e5e10cce53f24f446b9d85cfb4
dongqianggo <1012082932@qq.com>
2024-11-17 09:26:03 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -27,36 +27,36 @@ docker --version
 docker-compose --version
 
 
+echo " installed coin-prediction-reputer begin!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 git clone https://github.com/allora-network/coin-prediction-reputer
 cd coin-prediction-reputer
-
 cp config.example.json config.json
-
 chmod +x init.config
 ./init.config
-
 docker compose up -d
+cd ../
+echo " installed coin-prediction-reputer end!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-
-git clone https://github.com/allora-network/basic-coin-prediction-node
+echo " installed basic-coin-prediction-node begin!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+git clone https://github.com/dongqianggo/basic-coin-prediction-node.git
 cd basic-coin-prediction-node
-
 cp .env.example .env
-
+cp config.example.json config.json
 chmod +x init.config
 ./init.config
-
 docker compose up -d
+cd ../
+echo " installed basic-coin-prediction-node end!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
+echo " installed allora-offchain-node begin!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 git clone https://github.com/dongqianggo/allora-offchain-node.git
 cd allora-offchain-node.git
-
 cp .env.example .env
 cp config.example.json config.json
-
 chmod +x init.config
 ./init.config
-
 docker compose up -d
+cd ../
+echo " installed allora-offchain-node end!>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 echo "------------------------allora installed end!------------------------"
